07b0fe926dd3335a906574ce6ccba9802ffd4c9c
Mathieu Alorent <github@kumy.net>
2012-06-29 12:14:08 +0200

    added sourcecode link

--- lib/Parse/Arcconf.pm
+++ lib/Parse/Arcconf.pm
@@ -274,6 +274,10 @@ You can also look for information at:
 
 =over 4
 
+=item * Source code
+
+L<https://github.com/kumy/Parse-Arcconf>
+
 =item * RT: CPAN's request tracker
 
 L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Parse-Arcconf>
